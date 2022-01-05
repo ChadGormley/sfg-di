2405b2c958ac5114f0513dbe288d6c806c0ce3c1
chadgormley <chad.gormley@gmail.com>
2022-01-05 10:06:55 -0500

    Initial check-in of files

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        // this is injecting the service by the constructor
+        controller = new ConstructorInjectedController(new ConstructorGreetingService());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
