10a48dba1e76095279f74b5f1fbde7067b8a7158
chadgormley <chad.gormley@gmail.com>
2022-01-07 07:03:51 -0500

    Created a repository to mimic a database and used DI (dependency injection) to demonstrate how it can be done in the Java config.

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,8 +1,17 @@
 package guru.springframework.sfgdi.services;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+
 //@Profile("EN")  // causes spring to use this profile specified in the application.properties (if no "default" is specified anywhere else)
 //@Service("i18nService") // moved to Java configuration GreetingServiceCofig.java
 public class I18nEnglishGreetingService implements GreetingService {
+
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Hello World - EN";
